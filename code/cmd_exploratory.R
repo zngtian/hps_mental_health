3fbbc9da9650403cbdd17b8a384ed0726307f042
Zheng Tian <zngtian@gmail.com>
2021-03-20 09:13:54 -0400

    Revise cmd_preparedata.R.

--- code/cmd_exploratory.R
+++ code/cmd_exploratory.R
@@ -1,32 +1,4 @@
-source("cmd_initiate.R")
-
-####################  Prepare data ####################
-
-f <- "input_data/20210318_mental_health_aggregate.csv"
-df.mental <- read_csv(f)
-
-f <- "input_data/hps_phases.csv"
-df.wks <- read_csv(f)
-
-df.stfips <- fips_codes %>%
-    select(state.fips = state_code, state.abb = state, state = state_name) %>%
-    distinct()
-
-df.stfips <- rbind(df.stfips,
-                   tibble(state.fips = "99",
-                          state.abb = "US",
-                          state = "United States"))
-
-sp.state <- states(cb = TRUE, year = 2010)
-## sf.state <- st_as_sf(sp.state) %>%
-##     rename(state.fips = STATE, state = NAME)
-
-vars.meta <- c("state.fips", "week")
-
-df.use <- df.mental %>%
-    left_join(df.wks, by = "week") %>%
-    rename(state.fips = est_st) %>%
-    left_join(df.stfips, by = "state.fips")
+source("cmd_preparedata.R")
 
 
 ####################  Plotting ####################
--- code/cmd_initiate.R
+++ code/cmd_initiate.R
@@ -1,5 +1,23 @@
-library(tigris)
-library(readxl)
-library(lubridate)
-library(tidyverse)
+rm(list = ls())
+packages <- c("sf",
+              "tigris",
+              "lubridate",
+              "readxl",
+              "tidyverse")
 
+## Now load or install&load all
+package.check <- lapply(
+    packages,
+    FUN = function(x) {
+        if (!require(x, character.only = TRUE)) {
+            install.packages(x, dependencies = TRUE)
+            library(x, character.only = TRUE)
+        }
+    }
+)
+
+dirs <- c("input_data",
+          "output_data",
+          "output_img",
+          "output_files")
+tmp <- lapply(dirs, function(x) if (!dir.exists(x)) dir.create(x))
